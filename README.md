64803d83a9fd492b704bd09069bf240539f0f1e2
SamHekman <SamHekman@users.noreply.github.com>
2019-08-26 16:58:20 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Test01
+Test for longest palindromic substring in a string of numerical characters using CLI.
+
+Startby navigating to the directory containing "main.py" in a command line interface.
+Enter "python main.py" to run the application.
+Input the string in the empty line below and press the "enter" or "return" key.
+If a palendrome exists in the string, the longest palendrome will be output to the next line.
+The output will appear behind the promt as a result of using stdout instead of the print function.
+
+The instructions of the test call for a CLI application that uses input as stdin and stout.
+As means of input and output.
+
+The maximum input is a character lenth of 1,000,000.
+Only numeric characters 0-9 are permitted.
+* Note: The test instuctions have it listed as "1,000,0001,000,000", I am unsure if this is a typo.
+  
+The application will search for the longest palendromic substring within the input string.
+When finished, the application will return the longest palendromic substring in the next line behind the promt.
+
+
---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+import re 
+
+# Input str via command line using stdin.
+line = str(sys.stdin.readline()) 
+
+# Check if line variable is over the limit of 1,000,000 characters in length
+if len(line) > 1000000:
+	print("Input string exceeds character limit of 1,000,000")
+	sys.exit()
+
+# Check if line variable contains any character that is not a numerical character.
+elif not re.match("^[0-9]*$", line):
+	print("Input string contains unacceptable character types, all character must be 0-9")
+	sys.exit()
+
+def main(line):
+
+	# Build a repository to hold all palendromic substrings discovered.
+	result = []
+
+	# Select "chunk" as every possible substring in line str.
+	for ending_char in range(len(line)): 
+		for starting_char in range(0, ending_char):
+			chunk = line[starting_char:ending_char + 1] 
+
+
+            # Check if every possible substring is a palendrome,
+            # by checking the reverse of each substring against itself.
+			if chunk == chunk[::-1]: 
+				result.append(chunk)
+
+	# Returns longest palendrome using length as the search key.
+	return max(result, key = len)
+
+# output string to command line using stdout
+sys.stdout.write(main(line))
